ab9a3f3822427c9b35e7c8e92bd3a1c301b6cfb9
Anurag Rai <anuragrai5522@gmail.com>
2024-12-08 13:20:42 +0530

    first commit

---
+++ build/CMakeFiles/ogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ogen.dir/src/main.cpp.o"
+  "CMakeFiles/ogen.dir/src/main.cpp.o.d"
+  "ogen"
+  "ogen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ogen.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ogen.
+# This may be replaced when dependencies are built.
