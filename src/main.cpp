7e1708deecfde946b02b35aa209943a527e23484
Anurag Rai <anuragrai5522@gmail.com>
2024-12-09 23:12:48 +0530

    parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,46 @@
-#include <iostream>
 #include <fstream>
-#include <sstream>
+#include <iostream>
 #include <optional>
+#include <sstream>
 #include <vector>
 
-#include "./tokenization.hpp"
+#include "./generation.hpp"
 
-
-
-
-std::string tokens_to_ass(const std::vector<Token>& tokens){
-
-    std::stringstream output;
-    output << "global _start\n_start:\n";
-    for(int i = 0; i< tokens.size(); i++){
-        const Token& token = tokens.at(i);
-        if(token.type == TokenType::_exit){
-            if(i+1 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit){
-                if(i + 2 < tokens.size() && tokens.at(i + 2).type == TokenType::semi){
-                    output << "   mov rax, 60\n";
-                    output << "   mov rdi, " << tokens.at(i+1).value.value() << "\n";
-                    output << "   syscall";
-                }
-            }
-        }
-    }
-    return output.str();
-}
-
-
-int main(int argc, char *argv[]) {
-
-    if(argc != 2) {
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
         std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
-        std::cerr << "ogen <input.og>" << std::endl;
+        std::cerr << "hydro <input.hy>" << std::endl;
         return EXIT_FAILURE;
     }
 
-    std::string contents; {
-
+    std::string contents;
+    {
         std::stringstream contents_stream;
         std::fstream input(argv[1], std::ios::in);
         contents_stream << input.rdbuf();
         contents = contents_stream.str();
-
     }
 
     Tokenizer tokenizer(std::move(contents));
-
     std::vector<Token> tokens = tokenizer.tokenize();
+
+    Parser parser(std::move(tokens));
+    std::optional<NodeExit> tree = parser.parse();
+
+    if (!tree.has_value()) {
+        std::cerr << "No exit statement found" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    Generator generator(tree.value());
     {
-        std::fstream file("./out.asm", std::ios::out);
-        file << tokens_to_ass(tokens);
+        std::fstream file("out.asm", std::ios::out);
+        file << generator.generate();
     }
 
     system("nasm -felf64 out.asm");
     system("ld -o out out.o");
-
 
     return EXIT_SUCCESS;
 }
--- src/tokenization.hpp
+++ src/tokenization.hpp
@@ -1,97 +1,89 @@
-#pragma once //includes header file only once
-#include <iostream>
+#pragma once
+
 #include <string>
 #include <vector>
-#include <optional>
 
-
-enum class TokenType {
-    _exit,
+enum class TokenType { 
+    exit,
     int_lit,
-    semi
-};
+    semi 
+    };
 
 struct Token {
     TokenType type;
     std::optional<std::string> value {};
 };
 
-
 class Tokenizer {
     public:
-        inline explicit Tokenizer(std::string src) 
+        inline explicit Tokenizer(std::string src)
             : m_src(std::move(src))
         {
-
         }
 
-        inline std::vector<Token> tokenize() {
-
+        inline std::vector<Token> tokenize()
+        {
             std::vector<Token> tokens;
-
             std::string buf;
-
-            while(peak().has_value()) {
-                if(std::isalpha(peak().value())){
+            while (peak().has_value()) {
+                if (std::isalpha(peak().value())) {
                     buf.push_back(consume());
-                    while(peak().has_value() && std::isalnum(peak().value())){
+                    while (peak().has_value() && std::isalnum(peak().value())) {
                         buf.push_back(consume());
                     }
-                    if(buf == "exit") {
-                        tokens.push_back({.type = TokenType::_exit});
+                    if (buf == "exit") {
+                        tokens.push_back({ .type = TokenType::exit });
                         buf.clear();
                         continue;
                     }
-                    else{
-                        std::cerr << "Unknown token: " << buf << std::endl;
+                    else {
+                        std::cerr << "You messed up!" << std::endl;
                         exit(EXIT_FAILURE);
                     }
                 }
-                else if(peak().has_value() && std::isdigit(peak().value())) {
-
+                else if (std::isdigit(peak().value())) {
                     buf.push_back(consume());
-                    while(peak().has_value() && std::isdigit(peak().value())){
+                    while (peak().has_value() && std::isdigit(peak().value())) {
                         buf.push_back(consume());
                     }
-                    tokens.push_back({.type = TokenType::int_lit, .value = buf});
+                    tokens.push_back({ .type = TokenType::int_lit, .value = buf });
                     buf.clear();
                     continue;
                 }
-                else if(peak().value() == ';') {
+                else if (peak().value() == ';') {
                     consume();
-                    tokens.push_back({.type = TokenType::semi});
+                    tokens.push_back({ .type = TokenType::semi });
                     continue;
                 }
-                else if(std::isspace(peak().value())) {
+                else if (std::isspace(peak().value())) {
                     consume();
                     continue;
                 }
-                else{
-                    std::cerr << "Unknown token: " << peak().value() << std::endl;
+                else {
+                    std::cerr << "You messed up!" << std::endl;
                     exit(EXIT_FAILURE);
                 }
             }
             m_index = 0;
             return tokens;
-
         }
-        
 
     private:
-
-        [[nodiscard]]std::optional<char> peak(int ahead = 1) const {
-            if(m_index + ahead > m_src.length()){
+        [[nodiscard]] inline std::optional<char> peak(int ahead = 1) const
+        {
+            if (m_index + ahead > m_src.length()) {
                 return {};
             }
-            else{
+            else {
                 return m_src.at(m_index);
             }
         }
 
-        char consume() {
+        inline char consume()
+        {
             return m_src.at(m_index++);
         }
 
         const std::string m_src;
-        int m_index = 0;
+        size_t m_index = 0;
 };
