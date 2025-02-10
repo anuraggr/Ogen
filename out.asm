b2043cbe70b5d3da76b2ae349d8bb3edeb6b755a
Anurag Rai <anuragrai5522@gmail.com>
2025-02-10 15:33:33 +0530

    add example code snippits in readme

--- out.asm
+++ out.asm
@@ -1,34 +1,33 @@
 global _start
 _start:
-    mov rax, 1
+    mov rax, 10
     push rax
-    start_while_0:
+    mov rax, 20
+    push rax
     push QWORD [rsp + 0]
 
-    mov rax, 11
-    push rax
-    pop rbx
-    pop rax
-    cmp rax, rbx
-    jge end_while_1
-    mov rax, 1
-    push rax
-    push QWORD [rsp + 8]
+    push QWORD [rsp + 16]
 
     pop rax
     pop rbx
     add rax, rbx
     push rax
+    mov rax, 10
+    push rax
+    pop rbx
     pop rax
-    mov QWORD [rsp + 0], rax
-    add rsp, 0
-    jmp start_while_0
-    end_while_1:
-    push QWORD [rsp + 0]
-
+    cmp rax, rbx
+    je end_if_0
+    mov rax, 99
+    push rax
     mov rax, 60
     pop rdi
     syscall
+    add rsp, 0
+    jmp end_if_else_1
+    end_if_0:
+    add rsp, 0
+    end_if_else_1:
     mov rax, 60
     mov rdi, 0
     syscall
--- readme.md
+++ readme.md
@@ -6,3 +6,51 @@ Generates asm only for x86_64 for Linux.
 - **Operating System**: Linux
 - **Assembler**: NASM
 - **Linker**: LD
+
+
+## Example Code Snippets
+
+```
+let x = 10;
+
+for(let i = 0; i<x; i=i+1){
+    if(i==10){
+        exit(i);
+    }
+}
+
+exit(x);
+```
+
+```
+let x = 10;
+
+while(x>=1){
+    x = x-1;
+}
+
+exit(x);
+```
+```
+let variableName be 2;
+
+if(variableName==10){
+    exit(variableName);
+}
+elif(variableName==9){
+    exit(variableName);
+}
+else{
+    exit(1);
+}
+```
+```
+let var1 = 10;
+let var2 = 20;
+
+if(var1+var2==10){
+    exit(99);
+}
+#Exits with 0 because condition not true.
+#This is for comments.
+```
